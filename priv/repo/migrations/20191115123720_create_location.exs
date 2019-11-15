a26c0bc052ecd54165f9e30e7c65db2cfbc30320
Gaukhar Dauzhan <gaukhar.dauzhan@gmail.com>
2019-11-15 14:40:41 +0200

    added migration file for locations

---
+++ priv/repo/migrations/20191115123720_create_location.exs
@@ -0,0 +1,12 @@
+defmodule Parking.Repo.Migrations.CreateLocation do
+  use Ecto.Migration
+
+  def change do
+    create table(:locations) do
+      add :latitude, :float
+      add :longitude, :float
+      add :pricing_zone, :string
+      add :is_available, :boolean
+      timestamps()
+  end
+end
